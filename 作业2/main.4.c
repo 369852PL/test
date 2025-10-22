d20c9e08b688a979da3524febf8ab79565e277cc
369852PL <3446956895@qq.com>
2025-10-22 14:42:59 +0800

    Delete 作业2 directory

--- 作业2/main.4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
- int c1,c2;
- c1=321,c2=400;
- printf("%d,%d\n",c1,c2);
- printf("%c %c",c1,c2);
- return 0;
-}
--- 作业2/main.6.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
- float x,y;
- scanf("%f",&x);
- 
-}
